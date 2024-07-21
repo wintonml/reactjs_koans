6ab124d0b33f11109164355a2290b6f4e291010a
wintonml <wintonml@gmail.com>
2024-07-21 20:44:08 +0200

    Complete GroceryListPart3 koan

--- exercises/05-Challenge-GroceryList-part-3.jsx
+++ exercises/05-Challenge-GroceryList-part-3.jsx
@@ -41,10 +41,10 @@ class GroceryList extends React.Component {
     }
   }
 
-  // Fill the definition of the following method to allow clearing the list
-  // Hint: You can just simply set the groceries to an empty array.
   clearList() {
-    // Put your code here
+    if(this.state.groceries && this.state.groceries.length){
+        this.setState({groceries: []})
+    }
   }
 
   render() {
@@ -72,6 +72,7 @@ class GroceryList extends React.Component {
         </ul>
         {newProductInput}
         {newProductAddButton}
+        {clearListButton}
       </div>
     );
   }
